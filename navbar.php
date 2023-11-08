ba37a5fb266fb4fba8ee18d8cf559443d07bccc2
Aditya <bhandariaditya2004@gmail.com>
2023-11-08 13:22:57 +0530

    first commit

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://cdn.tailwindcss.com"></script>
+
+
+<link href="https://fonts.googleapis.com/css?family=Prosto+One&display=swap" rel="stylesheet">
+</head>
+<body>
+   <nav class="flex flex-row justify-between m-0 px-3 bg-[#000] text-white py-2">
+    <div>
+      <img src="Bank-modified.png" class="w-[50px]">
+      </div>
+      <div class="flex flex-row gap-6 font-md">
+        <a href="index.php" class="hover:bg-[#ffe] hover:text-black p-1 hover:font-semibold">Home</a>
+      
+        <a href="ViewCustomers.php" class="hover:bg-[#ffe] hover:text-black p-1 hover:font-semibold">Customers Details</a>
+      
+        <a href="Transfer.php" class="hover:bg-[#ffe] hover:text-black p-1 hover:font-semibold">Fund Transfer</a>
+      
+        <a href="RecordsPage.php" class="hover:bg-[#ffe] hover:text-black p-1 hover:font-semibold">Transaction History</a>
+      </div>
+   </nav>
+</body>
+</html>
